3b5823916dcaddda6629f6b8f08e17c5fa348952
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-10-18 14:27:01 +0100

    chapter 2 - numbers

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,12 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-
+    int num1 = 10;
     // declare a double variable with a value
-
+    double num2 = 2.6;
     // store the result of adding the two together in a variable
-
+    double sum = 20 + num1 + num2 ;
     // print it
-
+    System.out.println(sum);
   }
 }
