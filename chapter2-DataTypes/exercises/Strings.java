f82bff7fc7162c7ecfc3ae8884cf8a5e37f3b5f6
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-10-18 14:06:16 +0100

    chapter 2 - strings

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String name = "Shweta";
     // declare the second name
-
+    String name1 = "Chiu";
     // create your message
-
+    String message = String.format("%s and %s are learning Java!", name, name1);
     // print your message!
-
+    System.out.println(message);
   }
 }
